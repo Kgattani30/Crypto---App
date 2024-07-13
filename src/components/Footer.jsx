2936c69f208419e4dc3d9e7fda30207ba9f7bc98
Ankit <ankitsingh2000.15.1@gmail.com>
2024-07-13 20:23:36 +0530

    save change

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import {Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+
+
+const Footer = () => {
+  return (
+    <Box
+      bgColor={"blackAlpha.900"}
+      color={"whiteAlpha.700"}
+      minH={"48"}
+      px={"16"}
+      py={["16", "8"]}
+    >
+      <Stack h={"full"} direction={["column", "row"]} h={"full"} alignItems={"center"}>
+        <VStack w={"full"} alignItems={["center", "flex-start"]}>
+          <Text fontWeight={"bold"} color={"rgb(252,211,76)"}>
+            About Us
+          </Text>
+          <Text
+            fontSize={"sm"}
+            letterSpacing={"wide"}
+            textAlign={["center", "left"]}
+            w={["full", "60%"]}
+          >
+             I bring you a user-friendly crypto platform using the latest tech.
+            Real-time data, easy navigation and packed with features to help you
+            stay informed and make informed decisions. Thank you for choosing
+            cryptoRank.
+          </Text>
+        </VStack>
+        
+
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import btcSrc from "../assets/btc.png";
+import { motion } from "framer-motion";
+
+const Home = () => {
+  return (
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+      <motion.div
+        style={{
+          height: "80vh",
+        }}
+        animate={{
+          translateY: "20px",
+        }}
+        transition={{
+          duration: 1,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+        <Image
+       src={btcSrc} alt="cryptoRank" h={'full'} w={'full'} objectFit={'contain'} filter={'grayscale(1)'} 
+        />
+      </motion.div>
+
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={'rgb(252,211,76)'}
+        mt={"-25px"}
+      >
+        CryptoWorld
+      </Text>
+    </Box>
+  );
+};
+
+export default Home;
